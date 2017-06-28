c3f1a03da089b44a71457d7740abba577ab2196a
Marius Hamacher <marius.hamacher@stud.uni-due.de>
2017-06-28 15:15:56 +0200

    Refactor runbaseline.py. Handles multiple datasets and can be exported as module.

--- lemma_baseline/runbaseline.py
+++ lemma_baseline/runbaseline.py
@@ -1,11 +1,9 @@
 from baseline import Baseline
 import pandas as pd
 
-# TODO: scale for multiple datasets
-
-def evaluate(goldstandard, prediction):
+def evaluate(gold, prediction):
     result = ''
-    if goldstandard == prediction:
+    if gold == prediction:
         result += 'T'
     else:
         result += 'F'
@@ -15,41 +13,45 @@ def evaluate(goldstandard, prediction):
         result += 'N'
     return result
 
-headers = ['text', 'hypothesis', 'entailment']
-result_headers = ['actual', 'prediction', 'evaluation']
-lemma = Baseline()
+def run_lemma_baseline (dataframes, names=[], output_path='..\\resources\\output\\'):
+    result_headers = ['actual', 'prediction', 'evaluation']
+    results = []
+    metrics = []
+    lemma = Baseline()
+     
+    for df, name in zip(dataframes, names):
+        prediction = lemma.run(df.values)
+        evaluation = evaluation = (evaluate(gold, prediction) for gold, prediction in zip(df.entailment, prediction))
+        
+        result = pd.DataFrame(
+            list(zip(df.entailment, prediction, evaluation)),
+            columns=result_headers)
+        result.to_csv(output_path + 'lemma_baseline-' + name + '.csv')
+        results.append(result)
+        
+        confusion = result.evaluation.value_counts()
+        metric = pd.Series(
+            [
+                sum(confusion),
+                (confusion['TP'] + confusion['TN']) / sum(confusion),
+                confusion['TP'] / (confusion['TP'] + confusion['FN']),
+                confusion['TP'] / (confusion['TP'] + confusion['FP'])
+            ],
+            index=['total', 'accuracy', 'recall', 'precision'])
+        metric = confusion.append(metric)
+        metrics.append(metric)
+            
+    metrics_df = pd.DataFrame(metrics, index=names)
+    metrics_df['f1'] = 2 * metrics_df.recall * metrics_df.precision / (metrics_df.recall + metrics_df.precision)
+    metrics_df.to_csv(output_path + 'lemma_baseline-metrics.csv')
+    
+    return [results, metrics]
 
-### Dagan & Levy run
 
-daganlevy = pd.read_json('..\\resources\\datasets\\daganlevy.json').reindex(columns=headers).reset_index(drop=True)
-
-prediction = lemma.run(daganlevy.values)
-evaluation = (evaluate(gold, prediction) for gold, prediction in zip(daganlevy.entailment, prediction))
-
-result = pd.DataFrame(
-    list(zip(daganlevy.entailment, prediction, evaluation)),
-    columns=result_headers)
-
-confusion = result.evaluation.value_counts()
-
-acc = (confusion['TP'] + confusion['TN']) / sum(confusion)
-recall = confusion['TP'] / (confusion['TP'] + confusion['FN'])
-precision = confusion['TP'] / (confusion['TP'] + confusion['FP'])
-
-
-### Zeichner run
-
-zeichner = pd.read_json('..\\resources\\datasets\\zeichner.json').reindex(columns=headers).reset_index(drop=True)
-
-prediction = lemma.run(zeichner.values)
-evaluation = (evaluate(gold, prediction) for gold, prediction in zip(zeichner.entailment, prediction))
-
-result = pd.DataFrame(
-    list(zip(zeichner.entailment, prediction, evaluation)),
-    columns=result_headers)
-
-confusion = result.evaluation.value_counts()
-
-acc = (confusion['TP'] + confusion['TN']) / sum(confusion)
-recall = confusion['TP'] / (confusion['TP'] + confusion['FN'])
-precision = confusion['TP'] / (confusion['TP'] + confusion['FP'])
+# Only run, if called as script
+if __name__ == '__main__':
+    headers = ['text', 'hypothesis', 'entailment']
+    daganlevy = pd.read_json('..\\resources\\datasets\\daganlevy.json').reindex(columns=headers).reset_index(drop=True)
+    zeichner = pd.read_json('..\\resources\\datasets\\zeichner.json').reindex(columns=headers).reset_index(drop=True)
+    
+    run_lemma_baseline(dataframes=[daganlevy, zeichner], names=['daganlevy', 'zeichner'])
