52bff5f3531e00bd82ad7dcd17e7a9812943037d
Marius Hamacher <marius.hamacher@stud.uni-due.de>
2017-07-24 14:05:48 +0200

    Fix shared Predicates, add shared Attributes

--- dataset_comparison/stats.py
+++ dataset_comparison/stats.py
@@ -23,6 +23,13 @@ def predicates(dataset):
 def templates(dataset):
     return dataset.text.append(dataset.hypothesis).rename('templates')
 
+def attributes(dataset):
+    return dataset.tx.append([
+        dataset.ty,
+        dataset.hx,
+        dataset.hy,
+    ]).rename('attributes')
+
 def unique_predicates(dataset):
 	return set(predicates(dataset))
 
@@ -53,12 +60,12 @@ def template_coverage(datasetA, datasetB):
 def shared(datasetA, datasetB, column):
     A = set(datasetA[column])
     B = set(datasetB[column])
-    return A & B
+    return A.intersection(B)
 
 def shared_templates(datasetA, datasetB):
     templatesA = unique_templates(datasetA)
     templatesB = unique_templates(datasetB)
-    return templatesA & templatesB
+    return templatesA.intersection(templatesB)
 
 def shared_predicates_dataset(datasetA, datasetB):
     templatesB = unique_predicates(datasetB)
@@ -70,7 +77,7 @@ def shared_predicates_dataset(datasetA, datasetB):
 def shared_predicates(datasetA, datasetB):
     predicatesA = unique_predicates(datasetA)
     predicatesB = unique_predicates(datasetB)
-    return predicatesA & predicatesB
+    return predicatesA.intersection(predicatesB)
 
 def jaccard_index(listA, listB):
     A = set(listA)
@@ -102,24 +109,26 @@ def dataset_stats(dataset):
 	}))
 
 def descriptives(dataset):
-    temps = dataset.text.append(dataset.hypothesis).rename('templates')
-    preds = dataset.tpred.append(dataset.hypothesis).rename('predicates')
+    temps = templates(dataset)
+    preds = predicates(dataset)
+    attrs = attributes(dataset)
     return pd.DataFrame([
         temps.value_counts().describe(),
         dataset.text.value_counts().describe(),
         dataset.hypothesis.value_counts().describe(),
         preds.value_counts().describe(),
         dataset.tpred.value_counts().describe(),
-        dataset.hpred.value_counts().describe()
+        dataset.hpred.value_counts().describe(),
+        attrs.value_counts().describe()
         ])
+    
 
 def top10(series):
     return pd.DataFrame(series.value_counts()[:10])
 
-#TODO: relevance of templates unique to dataset per dataset
+#TODO: Optional relevance of templates unique to dataset per dataset
+#TODO: Optional relevance of templates, attributes, predicates shared by datasets (counts and stuff)
 
-#TODO: overlapping templates, attributes, predicates between datasets
-#TODO: relevance of templates, attributes, predicates shared by datasets (counts and stuff)
 #TODO: duplicates in hypothesis parts, daganlevy
 # poland was divided among russia 4690,4959
 # jupiter is big as the earth 1989, 13983, 14006
@@ -144,7 +153,7 @@ if __name__ == '__main__':
     # Comparison Stats
     df = pd.DataFrame(
         [dataset_stats(dataset) for dataset in dfs], 
-        index=datasets)
+        index=['daganlevy', 'zeichner'])
     df['shared_preds'] = [len(shared_predicates(daganlevy, zeichner))] * 2
     df['shared_templates'] = [len(shared_templates(daganlevy, zeichner))] * 2
     df['template_coverage'] = [template_coverage(daganlevy, zeichner), template_coverage(zeichner, daganlevy)]
@@ -153,24 +162,26 @@ if __name__ == '__main__':
     df['jaccard_templates'] = [jaccard_index(unique_templates(daganlevy), unique_templates(zeichner))] * 2
     df.T.to_csv(os.path.join(OUTPUT_PATH, 'dataset-stats.csv'))
     
-    #shared among datasets
+    # top10 shared among datasets
     daganlevy_shared = shared_predicates_dataset(daganlevy, zeichner)
     top10(templates(daganlevy_shared)).to_csv(os.path.join(OUTPUT_PATH, 'daganlevy_top10_shared_predicates(templateview).csv'))
     top10(predicates(daganlevy_shared)).to_csv(os.path.join(OUTPUT_PATH, 'daganlevy_top10_shared_predicates.csv'))
     
     zeichner_shared = shared_predicates_dataset(zeichner, daganlevy)
     top10(templates(zeichner_shared)).to_csv(os.path.join(OUTPUT_PATH, 'zeichner_top10_shared_predicates(templateview).csv'))
-    top10(predicates(daganlevy_shared)).to_csv(os.path.join(OUTPUT_PATH, 'zeichner_top10_shared_predicates.csv'))
+    top10(predicates(zeichner_shared)).to_csv(os.path.join(OUTPUT_PATH, 'zeichner_top10_shared_predicates.csv'))
     
     for name, dataset in datasets.items():
         outpath = os.path.join(OUTPUT_PATH, name)
         # Descriptives
         descriptives(dataset).to_csv(outpath + '_descriptives.csv')
         # Top10s
+        outpath = os.path.join(OUTPUT_PATH, 'top10s', name)
         top10(templates(dataset)).to_csv(outpath + '_top10_templates.csv')
         top10(dataset.text).to_csv(outpath + '_top10_texts.csv')
         top10(dataset.hypothesis).to_csv(outpath + '_top10_hypothesis.csv')
         top10(predicates(dataset)).to_csv(outpath + '_top10_predicates.csv')
         top10(dataset.tpred).to_csv(outpath + '_top10_tpreds.csv')
         top10(dataset.hpred).to_csv(outpath + '_top10_hpreds.csv')
+        top10(attributes(dataset)).to_csv(outpath + '_top10_attributes.csv')
     
