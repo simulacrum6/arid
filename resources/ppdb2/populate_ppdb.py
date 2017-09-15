3861e223a2f53b95d5c9c905bde215c34b935d53
Marius Hamacher <marius.hamacher@stud.uni-due.de>
2017-09-15 10:03:59 +0200

    Update DB generation

--- resources/ppdb2/populate_ppdb.py
+++ resources/ppdb2/populate_ppdb.py
@@ -2,21 +2,55 @@ import pandas as pd
 import sqlite3 as sql
 
 # use phrasal-l, due to memory restrictions on laptop
-databases = ['ppdb-2.0-xxxl-lexical', 'ppdb-2.0-l-phrasal']
-con = sql.connect('ppdb2.sqlite')
 
-for db in databases:
-    paraphrases = pd.read_csv(
-        db,
-        sep=' \|\|\| ',
-        iterator=True,
-        chunksize=50000,
-        names=[
-            'lhs', 'phrase', 'paraphrase', 'feature_value', 'alignment',
-            'entailment'
-        ])
+def has_vp(string):
+    if string.find('VP') != -1:
+        return True
+    else:
+        return False
 
-    for chunk in paraphrases:
-        chunk.to_sql('paraphrases', con, if_exists='append')
+def to_sql(databases):
+    for db in databases:
+        paraphrases = pd.read_csv(
+            db,
+            sep=' \|\|\| ',
+            iterator=True,
+            chunksize=50000,
+            names=[
+                'lhs', 'phrase', 'paraphrase', 'feature_value', 'alignment',
+                'entailment'
+            ],
+            usecols=['lhs', 'phrase', 'paraphrase', 'entailment'])
+
+        for chunk in paraphrases:
+            chunk = chunk[[entry in entailment_types for entry in chunk['entailment'].values]]
+            chunk = chunk[[has_vp(entry) for entry in chunk['lhs'].values]]
+            chunk[['phrase', 'paraphrase']].to_sql('paraphrases', con, if_exists='append')
 
 con.close()
+
+def to_csv(databases):
+    for db in databases:
+        paraphrases = pd.read_csv(
+            db,
+            sep=' \|\|\| ',
+            iterator=True,
+            chunksize=50000,
+            names=[
+                'lhs', 'phrase', 'paraphrase', 'feature_value', 'alignment',
+                'entailment'
+            ],
+            usecols=['lhs', 'phrase', 'paraphrase', 'entailment']
+        )
+        
+        for chunk in paraphrases:
+            chunk = chunk[[entry in entailment_types for entry in chunk['entailment'].values]]
+            chunk = chunk[[has_vp(entry) for entry in chunk['lhs'].values]]
+            chunk[['phrase', 'paraphrase']].to_csv('ppdb.csv', index=False)
+
+if __name__ == '__main__':
+    databases = ['ppdb-2.0-l-phrasal']
+    con = sql.connect('ppdb2.sqlite')
+    entailment_types = ['ReverseEntailment', 'ForwardEntailment', 'Equivalence']
+    
+    to_csv(databases)
